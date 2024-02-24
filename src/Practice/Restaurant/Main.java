7e20fc9fc0d6e0a9d7feca63c750e497cb397f79
KIS940 <inseon0806@gmail.com>
2024-02-24 21:19:55 +0900

    집에서 깃테스트 겸 작업

--- src/Practice/Restaurant/Main.java
+++ src/Practice/Restaurant/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Main {
 	private Order order = new Order();
 	private Scanner scanner = new Scanner(System.in);
-	
+	private boolean isMenuSetting = false;
+	private boolean isSameName = false;
+	private boolean isCheckMenuSetting = false;
+
 	public static void main(String[] args) {
 		Main main = new Main();
 		main.SettingMenu();
 	}
-	
+
 	private void SettingMenu()
 	{
-		boolean isMenuSetting = false;
-		boolean isSameName = false;
-
 		while(!isMenuSetting)
 		{
 			System.out.println("메뉴판을 설정해주세요.");
@@ -45,31 +45,30 @@ public class Main {
 			int price = scanner.nextInt();
 			System.out.println("3.메뉴 재고 수량");
 			int count = scanner.nextInt();
-			
+
 			order.SetMenu(name, price, count);
-			
+
 			System.out.println("메뉴판을 더 설정하시겠습까? Y/N");
 			String text = scanner.nextLine();
-			
-			if(text.equals("n") || text.equals("N"))
-			{
-				System.out.println("메뉴 설정이 끝났습니다.");
-				isMenuSetting = true;
-			}
-			else if(text.equals("y") || text.equals("Y"))
-			{
-						
-			}
-			else
-			{
-				System.out.println("잘못 입력하셨습니다 다시 입력하세요.");
-			}
+
 		}
 	}
-	
+
+	private boolean CheckMenu(String text)
+	{
+		if(text.equals("n") || text.equals("N"))
+		{
+			System.out.println("메뉴 설정이 끝났습니다.");
+			isMenuSetting = true;
+		}
+		else if(text.equals("y") || text.equals("Y"))
+		{
+		}
+	}
+
 	private void ShowMenu()
 	{
-		
+
 	}
 
 }
