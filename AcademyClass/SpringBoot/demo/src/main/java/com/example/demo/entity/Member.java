b1ad21b1d7b76f5c3143e2e19fa809ba42c17d7c
KIS940 <inseon0806@gmail.com>
2024-04-02 14:45:49 +0900

    스프링부트 수업2일차

--- AcademyClass/SpringBoot/demo/src/main/java/com/example/demo/entity/Member.java
+++ AcademyClass/SpringBoot/demo/src/main/java/com/example/demo/entity/Member.java
@@ -4,21 +4,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
-@AllArgsConstructor
-@ToString
-@NoArgsConstructor
-public class Article {
+public class Member {
+
     @Id
     @GeneratedValue
     private Long id;
     @Column
-    private String title;
+    private String email;
     @Column
-    private String content;
+    private String password;
+
+    public Member(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 
 }
