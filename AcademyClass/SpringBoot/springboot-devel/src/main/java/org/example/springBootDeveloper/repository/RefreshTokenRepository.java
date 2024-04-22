8fdc1f44443e485ebca91b03349a741de4be5d77
KIS940 <inseon0806@gmail.com>
2024-04-22 18:01:26 +0900

    스프링 15일

---
+++ AcademyClass/SpringBoot/springboot-devel/src/main/java/org/example/springBootDeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package org.example.springBootDeveloper.repository;
+
+import org.example.springBootDeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+}
---
+++ AcademyClass/SpringBoot/springboot-devel/src/main/java/org/example/springBootDeveloper/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package org.example.springBootDeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springBootDeveloper.domain.RefreshToken;
+import org.example.springBootDeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken){
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
