2e98efc169993d9378eefe0a96ef78a84496c9ca
KIS940 <inseon0806@gmail.com>
2024-05-05 17:01:36 +0900

    파일정리 및 아이디찾기

--- Practice/TemporaryTeamProject/gaxga/src/main/java/com/teamproject/gaxga/controller/FindInfoController.java
+++ Practice/TemporaryTeamProject/gaxga/src/main/java/com/teamproject/gaxga/controller/FindInfoController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
@@ -22,19 +20,25 @@ public class FindInfoController {
         return "public/accountManagement/findInfo";
     }
 
-    @PostMapping("/findInfo")
-    public String findId(@RequestParam("user_Name") String nick, @RequestParam("userId_PhoneNumber") String phoneNum, Model model) {
-        log.info("=======nick: {}, phoneNum: {}", nick, phoneNum);
-        User user = findInfoService.findUserInfo(nick, phoneNum);
+//    @PostMapping("/findInfo")
+//    public String findId(@RequestParam("user_Name") String nick, @RequestParam("userId_PhoneNumber") String phoneNum, Model model) {
+//        log.info("=======nick: {}, phoneNum: {}", nick, phoneNum);
+//        User user = findInfoService.findUserInfo(nick, phoneNum);
+//
+//        if (user != null) {
+//            log.info("=======찾은 정보 userId: {}", user.getGaId());
+//            model.addAttribute("userId","찾은 아이디: " + user.getGaId());
+//            return "redirect:/login";
+//        } else {
+//            log.info("사용자를 찾을 수 없습니다.");
+//            model.addAttribute("userId", "사용자를 찾을 수 없음");
+//            return "redirect:/findInfo";
+//        }
+//    }
 
-        if (user != null) {
-            log.info("=======찾은 정보 userId: {}", user.getGaId());
-            model.addAttribute("userId","찾은 아이디: " + user.getGaId());
-            return "redirect:/login";
-        } else {
-            log.info("사용자를 찾을 수 없습니다.");
-            model.addAttribute("userId", "사용자를 찾을 수 없음");
-            return "redirect:/findInfo";
-        }
+    @PostMapping("/findInfo/ID")
+    @ResponseBody
+    public String findId(@RequestBody User user){
+        return "public/accountManagement/findInfo";
     }
 }
--- Practice/TemporaryTeamProject/gaxga/src/main/resources/static/js/findInfo.js
+++ Practice/TemporaryTeamProject/gaxga/src/main/resources/static/js/findInfo.js
@@ -1,19 +1,41 @@
 // 아이디 찾기
-const findIdBtn = document.getElementById("findIdBtn");
-if(findIdBtn){
-    findIdBtn.addEventListener("click", event=>{
-        const userId = document.getElementById("userId").value;
-        console.log(userId);
-        if (userId !== "사용자를 찾을 수 없음") {
+// const findIdBtn = document.getElementById("findIdBtn");
+const findId = () =>{
+    const userNick = document.getElementById("user_Name").value;
+    const userPhone = document.getElementById("userId_PhoneNumber").value;
+    const info = {
+        "userNick" : userNick,
+        "userPhone" : userPhone,
+    };
 
-            alert("찾은 아이디: " + userId);// 아이디를 찾았을 때
-        } else {
-            alert("사용자를 찾을 수 없습니다." + userId); // 아이디를 찾지 못했을 때
+    $.ajax({
+        type: "post",
+        url: "/findInfo/ID",
+        data:JSON.stringify(info),
+        contentType: "application/json",
+        success: function (res){
+            console.log("성공", res);
+        },
+        error: function(){
+            console.log("실패")
         }
-
-    });
-
+    })
 }
+//
+// if(findIdBtn){
+//     findIdBtn.addEventListener("click", event=>{
+//         const userId = document.getElementById("userId").value;
+//         console.log(userId);
+//         if (userId !== "사용자를 찾을 수 없음") {
+//
+//             alert("찾은 아이디: " + userId);// 아이디를 찾았을 때
+//         } else {
+//             alert("사용자를 찾을 수 없습니다." + userId); // 아이디를 찾지 못했을 때
+//         }
+//
+//     });
+//
+// }
 
 
 // 비밀번호 찾기
